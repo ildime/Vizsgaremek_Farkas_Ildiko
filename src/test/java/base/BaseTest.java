028ad03ed5d5fe461aedc08f8e5b72cf2c72b77c
ildime <toth.ildi1@gmail.com>
2021-08-19 13:38:50 +0200

    version15.0

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,8 +1,11 @@
 package base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.TestWatcher;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import pages.HomePage;
 import utils.Utils;
 
@@ -19,7 +22,24 @@ public class BaseTest {   //implement TestWatcher?
 
     @BeforeEach
     public void Setup() {
-        driver = Utils.getDriver();
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        //options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--incognito");
+        //options.setHeadless(true);
+        //options.setExperimentalOption("excludeSwitches", "disable-popup-blocking");
+        // driver.manage().window().maximize();
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-extensions");
+        options.addArguments("start-maximized");
+        //options.addArguments("--disable-dev-shm-usage");
+        //options.addArguments("--proxy-server='direct://");
+        //options.addArguments("--proxy-bypass-list=*");
+        //options.addArguments("--disable-gpu");
+        driver = new ChromeDriver(options);
         driver.get("https://skinsmart.hu/");
         HomePage.clickAcceptCookies();
     }
